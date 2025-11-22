dd5e4709fc3a55814da053c5461a1efd8a0a83f6
myuon <ioi.joi.koi.loi@gmail.com>
2025-11-23 01:14:06 +0900

    feat: mse

--- src/dpl/functions/__init__.py
+++ src/dpl/functions/__init__.py
@@ -3,3 +3,5 @@ from dpl.functions.reshape import reshape
 from dpl.functions.transpose import transpose
 from dpl.functions.sum import sum
 from dpl.functions.matmul import matmul
+from dpl.functions.mean_squared_error import mean_squared_error
+from dpl.functions.broadcast_to import broadcast_to, sum_to
---
+++ src/dpl/functions/mean_squared_error.py
@@ -0,0 +1,29 @@
+import numpy as np
+from dpl.core import Variable, Function, as_variable
+
+
+class MeanSquaredError(Function):
+    def apply(self, y_pred: Variable, y_true: Variable) -> Variable:
+        result = super().__call__(y_pred, y_true)
+        assert isinstance(result, Variable)
+        return result
+
+    def forward(self, *xs: np.ndarray) -> np.ndarray:
+        x0, x1 = xs
+        diff = x0 - x1
+        return (diff**2).sum() / diff.size
+
+    def backward(self, *gys: Variable) -> tuple[Variable, Variable]:
+        from dpl.functions.broadcast_to import broadcast_to
+
+        x0, x1 = self.inputs
+        diff = x0 - x1
+        (gy,) = gys
+        gy = broadcast_to(gy, diff.shape)
+        gx0 = gy * diff * (2.0 / diff.size)
+        gx1 = -gx0
+        return gx0, gx1
+
+
+def mean_squared_error(y_pred: Variable, y_true: Variable) -> Variable:
+    return MeanSquaredError().apply(y_pred, y_true)
